b08edc1e3a8607705bae6ae02a3933b3a2835f1e
devfle <52854338+devfle@users.noreply.github.com>
2023-08-12 23:24:59 +0200

    feat: add remove icon to swipe animation

--- components/ListItemTodo.tsx
+++ components/ListItemTodo.tsx
@@ -1,6 +1,7 @@
+import { View, StyleSheet } from 'react-native';
 import { Gesture, GestureDetector } from 'react-native-gesture-handler';
-import { List, Chip } from 'react-native-paper';
-import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
+import { List, Chip, IconButton } from 'react-native-paper';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 interface ListItemTodoProps {
   title: string;
@@ -11,12 +12,26 @@ interface ListItemTodoProps {
 
 function ListItemTodo({ title, text, icon, state }: ListItemTodoProps) {
   const translateX = useSharedValue(0);
+  const opacity = useSharedValue(0);
+
+  const styles = StyleSheet.create({
+    deleteIcon: {
+      position: 'absolute',
+      right: 16,
+      top: 8,
+    },
+  });
+
   const panGesture = Gesture.Pan()
     .onUpdate((event) => {
       translateX.value = event.translationX;
+
+      if (event.translationX < -40) opacity.value = withTiming(1);
+      if (opacity.value === 1 && event.translationX > -40) opacity.value = withTiming(0);
     })
     .onEnd(() => {
       translateX.value = 0;
+      opacity.value = 0;
     });
 
   const slideStyle = useAnimatedStyle(() => ({
@@ -24,11 +39,16 @@ function ListItemTodo({ title, text, icon, state }: ListItemTodoProps) {
   }));
 
   return (
-    <GestureDetector gesture={panGesture}>
-      <Animated.View style={slideStyle}>
-        <List.Item title={title} description={text} left={(props) => <List.Icon {...props} icon={icon} />} right={(props) => <Chip {...props}>{state}</Chip>} />
+    <View>
+      <GestureDetector gesture={panGesture}>
+        <Animated.View style={slideStyle}>
+          <List.Item title={title} description={text} left={(props) => <List.Icon {...props} icon={icon} />} right={(props) => <Chip {...props}>{state}</Chip>} />
+        </Animated.View>
+      </GestureDetector>
+      <Animated.View style={[styles.deleteIcon, { opacity }]}>
+        <IconButton icon="delete" iconColor="#dd0000" />
       </Animated.View>
-    </GestureDetector>
+    </View>
   );
 }
 
